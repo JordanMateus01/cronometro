fc2e5cb52e95758c647f1393571a0848959b1b49
Ana <ana@example.com>
2023-05-03 12:09:40 -0300

    Enviando os arquivos referentes ao código do cronômetro

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// new date(122132313);
+function relogio(){
+    function getTimeFromSeconds(seconds){
+        const data = new Date(seconds * 1000);
+        return data.toLocaleTimeString("pt-BR", {hour12: false, timeZone: "GMT"});
+    }
+    
+    
+    const relogio = document.querySelector(".relogio");
+    let seconds = 0;
+    let timer;
+    
+    function startWatch(){
+        timer = setInterval(function() {seconds++; relogio.innerHTML = getTimeFromSeconds(seconds) }, 1000);
+    }
+    
+    document.addEventListener("click", function(e){
+        const el = e.target;
+        
+        if(el.classList.contains("zerar")){
+            clearInterval(timer);
+            relogio.innerHTML = "00:00:00";
+            relogio.classList.remove("pausado");
+            seconds = 0;
+        }
+    
+        if (el.classList.contains("pausar")){
+            clearInterval(timer);
+            relogio.classList.add("pausado");    
+        }
+    
+        if(el.classList.contains("iniciar")){
+            relogio.classList.remove("pausado");
+            clearInterval(timer);
+            startWatch();
+        }
+    });
+}
+relogio();
